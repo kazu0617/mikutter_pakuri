eedcc5789c748831fb0a7938c54d0fc68d976fdb
kazu0617 <kazuyagi19990617@hotmail.co.jp>
2016-06-25 01:11:31 +0900

    構造を刷新した

--- mikutter_pakuri.rb
+++ mikutter_pakuri.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-Plugin.create(:pakuri) do
-  command(:pakuri,
-          name: "ぱくる",
+Plugin.create :mikutter_pakuri do
+  command(:rtpakuri,
+          name: "ふぁぼぱく",
           condition: Plugin::Command[:CanReplyAll],
           visible: true,
           role: :timeline ) do |m|
@@ -12,9 +12,7 @@ Plugin.create(:pakuri) do
   def pakuri(message)
     name=message.idname
     message.favorite(true)
-    message.retweet
     str=message.to_s
     Post.primary_service.update(:message => str,:replyto => message[:replyto])
-    Post.primary_service.update(:message => "pakuri.rbによりhttp://twitter.com/#!/#{name}/status/#{message.id}をパクリました")
   end
 end
